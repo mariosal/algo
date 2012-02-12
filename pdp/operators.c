4bd99f215114b3bd7da1a75e8fdd203a19b198fb
Mario Saldinger <mariosaldinger@gmail.com>
2012-02-13 00:19:03 +0200

    second round juniors\

---
+++ pdp/operators.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int n, i, j, min[ 2 ], *a;
+
+    freopen( "operators.in", "r", stdin );
+    freopen( "operators.out", "w", stdout );
+
+    scanf( "%d", &n );
+    a = ( int* )malloc( n * sizeof( int ) );
+
+    for ( i = 0; i < n; i += 1 ) {
+        scanf( "%d", &a[ i ] );
+    }
+
+    i = 0;
+    j = n - 1;
+    min[ 0 ] = a[ i ];
+    min[ 1 ] = a[ j ];
+    while ( i < j ) {
+        if ( abs( min[ 0 ] + min[ 1 ] ) > abs( a[ i ] + a[ j ] ) ) {
+            min[ 0 ] = a[ i ];
+            min[ 1 ] = a[ j ];
+        }
+        if ( a[ i ] + a[ j ] > 0 ) {
+            j -= 1;
+        }
+        else {
+            i += 1;
+        }
+    }
+
+    printf( "%d %d\n", min[ 0 ], min[ 1 ] );
+
+    return 0;
+}
