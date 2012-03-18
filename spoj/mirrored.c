77853d3ed9c64ec465cd764ab44d36d36e4bf370
Mario Saldinger <mariosaldinger@gmail.com>
2012-03-18 16:02:19 +0200

    arith2 mirrored

---
+++ spoj/mirrored.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char i, j;
+
+    puts( "Ready" );
+    while ( 1 ) {
+        i = getchar();
+        j = getchar();
+        getchar();
+
+        if ( i == ' ' ) {
+            break;
+        }
+
+        if ( ( i + j == 'b' + 'd' ) || ( i + j == 'p' + 'q' ) ) {
+            puts( "Mirrored pair" );
+        }
+        else {
+            puts( "Ordinary pair" );
+        }
+    }
+
+    return 0;
+}
