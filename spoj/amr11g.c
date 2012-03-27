a166a6ff67103694fe3aca977e947015617bdb76
Mario Saldinger <mariosaldinger@gmail.com>
2012-03-27 14:51:37 +0300

    amr11g, sizecon

---
+++ spoj/amr11g.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char seg[ 51 ];
+    short t;
+
+    scanf( "%hd", &t );
+    while ( t-- ) {
+        scanf( "%s", seg );
+        if ( strchr( seg, 'D' ) == NULL ) {
+            printf( "Possible\n" );
+        }
+        else {
+            printf( "You shall not pass!\n" );
+        }
+    }
+
+    return 0;
+}
