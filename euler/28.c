27b52933fd2e27277eec2c4472dd95e720fcce7f
Mario Saldinger <mariosaldinger@gmail.com>
2012-03-28 19:36:53 +0300

    28, 32 euler

---
+++ euler/28.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+ * center: 1
+ * upright: n^2
+ * upleft: upright -( n -1 )
+ * downleft: upleft -( n -1 ) 
+ * downright: downleft -( n -1 )
+ */
+
+#define N 1001
+
+int main() {
+    int i, sum;
+
+    sum = 1;
+    for ( i = 3; i <= N; i += 2 ) {
+        sum += i * i;
+        sum += i * i + -1 * ( i - 1 );
+        sum += i * i + -2 * ( i - 1 );
+        sum += i * i + -3 * ( i - 1 );
+    }
+    printf( "%d\n", sum );
+
+    return 0;
+}
---
+++ euler/32.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+long long toi( char *a, char from, char to ) {
+    long long n;
+
+    n = 0;
+    while ( from <= to ) {
+        n *= 10;
+        n += a[ from ];
+        ++from;
+    }
+
+    return n;
+}
+
+int main() {
+    char i, j, a[ 9 ] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    unsigned short ans;
+    int multiplicand, multiplier, product;
+    set< int > mark;
+
+    ans = 0;
+    do {
+        for ( i = 0; i <= 6; ++i ) {
+            for ( j = i + 1; j <= 7; ++j ) {
+                multiplicand = toi( a, 0, i );
+                multiplier = toi( a, i + 1, j );
+                product = toi( a, j + 1, 8 );
+
+                if ( multiplicand * multiplier == product ) {
+                    if ( mark.insert( product ).second ) {
+                        ans += product;
+                    }
+                }
+            }
+        }
+    } while ( next_permutation( a, a + 9 ) );
+    printf( "%hu\n", ans );
+
+    return 0;
+}
