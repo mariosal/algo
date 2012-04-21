47e5d78206b211f5caf7143d35269d75c4c98d85
Mario Saldinger <mariosaldinger@gmail.com>
2012-04-22 01:24:19 +0300

    faster reverse

--- euler/145.c
+++ euler/145.c
@@ -2,26 +2,14 @@
 #include <math.h>
 
 int reverse( int n ) {
-    char i, len, a, b;
-    int down, up;
-
-    len = floor( log10( n ) ) + 1;
-    down = pow( 10, len - 1 );
-    up = 1;
-    for ( i = 0; i < len / 2; ++i ) {
-        a = n / down % 10;
-        b = n / up % 10;
-
-        n -= ( int )a * down;
-        n += ( int )b * down;
-
-        n -= b * up;
-        n += a * up;
-
-        down /= 10;
-        up *= 10;
+    int i;
+    i = 0;
+    while ( n ) {
+        i *= 10;
+        i += n % 10;
+        n /= 10;
     }
-    return n;
+    return i;
 }
 
 char hasOdd( int n ) {
@@ -36,11 +24,12 @@ char hasOdd( int n ) {
 
 int main() {
     const int N = 1000000000;
-    int i, ans;
+    int i, ri, ans;
 
     ans = 0;
     for ( i = 1; i < N; ++i ) {
-        if ( hasOdd( i + reverse( i ) ) && floor( log10( i ) ) == floor( log10( reverse( i ) ) ) ) {
+        ri = reverse( i );
+        if ( hasOdd( i + ri ) && ( int )log10( i ) == ( int )log10( ri ) ) {
             ++ans;
         }
     }
