0d2616c32c0ff71f8eb524eb3033623f310f3031
Mario Saldinger <mariosaldinger@gmail.com>
2012-05-09 15:33:15 +0300

    Box Factory top-down dp

--- codejam/2012/1c/box.cpp
+++ codejam/2012/1c/box.cpp
@@ -1,90 +1,74 @@
 #include <cstdio>
-#include <vector>
-#include <stack>
+#include <map>
 #include <algorithm>
 
 using namespace std;
 
+typedef pair< pair< char, char >, pair< long, long > > state;
+
 typedef struct {
-    long long amount;
+    long amount;
     short type;
 } item;
 
-typedef struct {
-    unsigned long long best;
-    short i;
-    short j;
-    vector< item > box;
-    vector< item > toy;
-} state;
+long rdp( item *box, char cntBox, item *toy, char cntToy, char i, char j, long takeI, long takeJ ) {
+    long ans;
+    state key;
+    static map< state, long > memo;
+    map< state, long >::iterator it;
 
-unsigned long long bf( state top ) {
-    unsigned long long best;
-    state next;
-    stack< state > s;
-
-    best = 0;
-    s.push( top );
-    while ( !s.empty() ) {
-        top = s.top();
-        s.pop();
-
-        if ( best < top.best ) {
-            best = top.best;
-        }
-        if ( top.i >= top.box.size() || top.j >= top.toy.size() ) {
-            continue;
-        }
-
-        next = top;
-        if ( next.box[ next.i ].type == next.toy[ next.j ].type ) {
-            if ( next.box[ next.i ].amount > next.toy[ next.j ].amount ) {
-                next.best += ( unsigned long long )next.toy[ next.j ].amount;
-                next.box[ next.i ].amount -= next.toy[ next.j ].amount;
-                ++next.j;
-            }
-            else {
-                next.best += ( unsigned long long )next.box[ next.i ].amount;
-                next.toy[ next.j ].amount -= next.box[ next.i ].amount;
-                ++next.i;
-            }
-            s.push( next );
-            continue;
-        }
-
-        next = top;
-        ++next.i;
-        s.push( next );
-
-        next = top;
-        ++next.j;
-        s.push( next );
+    if ( i >= cntBox || j >= cntToy ) {
+        return 0;
     }
 
-    return best;
+    key = make_pair( make_pair( i, j ), make_pair( takeI, takeJ ) );
+    it = memo.find( key );
+    if ( it != memo.end() ) {
+        return it->second;
+    }
+
+    if ( box[ i ].type == toy[ j ].type ) {
+        if ( box[ i ].amount - takeI > toy[ j ].amount - takeJ ) {
+            ans = toy[ j ].amount - takeJ + rdp( box, cntBox, toy, cntToy, i, j + 1, takeI + toy[ j ].amount - takeJ, 0 );
+        }
+        else {
+            ans = box[ i ].amount - takeI + rdp( box, cntBox, toy, cntToy, i + 1, j, 0, takeJ + box[ i ].amount - takeI );
+        }
+    }
+    else {
+        ans = max(
+            rdp( box, cntBox, toy, cntToy, i + 1, j, 0, takeJ ),
+            rdp( box, cntBox, toy, cntToy, i, j + 1, takeI, 0 )
+        );
+    }
+
+    if ( !i && !j ) {
+        memo.clear();
+        return ans;
+    }
+    return memo[ key ] = ans;
 }
 
 int main() {
     short t, i, j, cntBox, cntToy;
-    state top;
+    item *box, *toy;
 
     scanf( "%hd", &t );
     for ( i = 1; i <= t; ++i ) {
         scanf( "%hd %hd", &cntBox, &cntToy );
-        top.box.resize( cntBox );
-        top.toy.resize( cntToy );
+        box = new item[ cntBox ];
+        toy = new item[ cntToy ];
 
         for ( j = 0; j < cntBox; ++j ) {
-            scanf( "%lld %hd", &top.box[ j ].amount, &top.box[ j ].type );
+            scanf( "%ld %hd", &box[ j ].amount, &box[ j ].type );
         }
         for ( j = 0; j < cntToy; ++j ) {
-            scanf( "%lld %hd", &top.toy[ j ].amount, &top.toy[ j ].type );
+            scanf( "%ld %hd", &toy[ j ].amount, &toy[ j ].type );
         }
+        printf( "Case #%hd: %ld\n", i, rdp( box, cntBox, toy, cntToy, 0, 0, 0, 0 ) );
 
-        top.best = 0;
-        top.i = 0;
-        top.j = 0;
-        printf( "Case #%hd: %llu\n", i, bf( top ) );
+        delete[] box;
+        delete[] toy;
     }
 
     return 0;
