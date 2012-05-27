5ead93e708d4bf8365e033ee7c5dc0064df11dd1
Mario Saldinger <mariosaldinger@gmail.com>
2012-05-27 18:01:39 +0300

    improved 69, mathematical approach

--- euler/69.cpp
+++ euler/69.cpp
@@ -1,67 +1,12 @@
 #include <cstdio>
-#include <cmath>
-
-bool *sieve( int n ) {
-    bool *isPrime;
-    int i, j;
-
-    isPrime = new bool[ n + 1 ];
-    isPrime[ 0 ] = false;
-    isPrime[ 1 ] = false;
-    for ( i = 2; i <= n; ++i ) {
-        isPrime[ i ] = true;
-    }
-    for ( i = 2; i * i <= n; ++i ) {
-        if ( isPrime[ i ] ) {
-            for ( j = i * i; j <= n; j += i ) {
-                isPrime[ j ] = false;
-            }
-        }
-    }
-
-    return isPrime;
-}
-
-int phi( int n, int *prime, int cntPrime ) {
-    int i;
-    float product;
-
-    product = 1;
-    for ( i = 0; i < cntPrime; ++i ) {
-        if ( prime[ i ] > n ) {
-            break;
-        }
-        if ( n % prime[ i ] == 0 ) {
-            product *= 1 - ( float )1 / prime[ i ];
-        }
-    }
-
-    return ceil( n * product );
-}
 
 int main() {
-    const int N = 1000000;
-    bool *isPrime;
-    int i, dummy, ans, cntPrime, *prime, *memo;
-    float maxFraction;
+    char i, prime[ 7 ] = { 2, 3, 5, 7, 11, 13, 17 };
+    int ans;
 
-    isPrime = sieve( N );
-    prime = new int[ N ];
-    cntPrime = 0;
-    for ( i = 2; i <= N; ++i ) {
-        if ( isPrime[ i ] ) {
-            prime[ cntPrime ] = i;
-            ++cntPrime;
-        }
-    }
-
-    maxFraction = 2; // 2 / φ( 2 )
-    for ( i = 2; i <= N; ++i ) {
-        dummy = phi( i, prime, cntPrime );
-        if ( maxFraction < ( float )i / dummy ) {
-            maxFraction = ( float )i / dummy;
-            ans = i;
-        }
+    ans = 1;
+    for ( i = 0; i < 7; ++i ) {
+        ans *= prime[ i ];
     }
     printf( "%d\n", ans );
 
