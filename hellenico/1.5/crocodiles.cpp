60c98e9bdb92c948694d09c06f1f36d8327bf2b5
Mario Saldinger <mariosaldinger@gmail.com>
2012-07-06 16:25:14 +0300

    crocodiles

---
+++ hellenico/1.5/crocodiles.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int i, j, k, sum, maxSum, n, a[ 200001 ];
+
+    freopen( "crocodiles.in", "r", stdin );
+    freopen( "crocodiles.out", "w", stdout );
+
+    for ( i = 0; i <= 200000; ++i ) {
+        a[ i ] = 0;
+    }
+    
+    scanf( "%d", &n );
+    for ( i = 0; i < n; ++i ) {
+        scanf( "%d %d", &j, &k );
+        j += 100000;
+        k += 100000;
+        ++a[ j ];
+        --a[ k ];
+    }
+
+    sum = maxSum = 0;
+    for ( i = 0; i <= 200000; ++i ) {
+        sum = max( sum + a[ i ], a[ i ] );
+        maxSum = max( maxSum, sum );
+    }
+    printf( "%d\n", maxSum );
+
+    return 0;
+}
