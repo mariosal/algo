b28a57b1afc6e2ea763c34d8533139fb75f8cd38
Mario Saldinger <mariosaldinger@gmail.com>
2012-10-31 17:59:22 -0400

    rebels 50%: Correct solution, ternary search

--- hellenico/contest/oct12/rebels.cpp
+++ hellenico/contest/oct12/rebels.cpp
@@ -1,10 +1,48 @@
 #include <cstdio>
-#include <cmath>
+#include <cstdlib>
+
+short n;
+int truck, *rebel;
+
+long long f( int x ) {
+  short i;
+  long long ret;
+
+  ret = llabs( ( long long )truck - x );
+  for ( i = 0; i < n; ++i ) {
+    ret += llabs( ( long long )rebel[ i ] - x );
+  }
+  return ret;
+}
+
+long long tSearch( int p, int s ) {
+  int q, r;
+  long long ret;
+
+  while ( ( long long )s - p >= 3 ) {
+    q = ( 2LL * p + s ) / 3;
+    r = ( p + 2LL * s ) / 3;
+
+    if ( f( q ) < f( r ) ) {
+      s = r;
+    }
+    else {
+      p = q;
+    }
+  }
+
+  ret = f( p );
+  for ( ++p; p <= s; ++p ) {
+    if ( ret > f( p ) ) {
+      ret = f( p );
+    }
+  }
+  return ret;
+}
 
 int main() {
-  short n;
-  int i, sum, *rebel;
-  float truck, ans;
+  short i;
+  int minCoord, maxCoord;
 
   freopen( "rebels.in", "r", stdin );
   freopen( "rebels.out", "w", stdout );
@@ -12,18 +50,18 @@ int main() {
   scanf( "%*c %hd", &n );
   rebel = new int[ n ];
 
-  sum = 0;
-  scanf( "%f", &truck );
+  scanf( "%d", &truck );
+  minCoord = maxCoord = truck;
   for ( i = 0; i < n; ++i ) {
     scanf( "%d", &rebel[ i ] );
-    sum += rebel[ i ];
+    if ( minCoord > rebel[ i ] ) {
+      minCoord = rebel[ i ];
+    }
+    if ( maxCoord < rebel[ i ] ) {
+      maxCoord = rebel[ i ];
+    }
   }
+  printf( "%lld\n", tSearch( minCoord, maxCoord ) );
   
-  ans = fabs( truck - ( float )sum / n );
-  for ( i = 0; i < n; ++i ) {
-    ans += fabs( rebel[ i ] - ( float )sum / n );
-  }
-  printf( "%.0lf\n", ans );
-
   return 0;
 }
