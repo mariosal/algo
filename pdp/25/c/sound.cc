8f26291309594d923668805d1462455f7e4246cb
Mario Saldinger <mariosaldinger@gmail.com>
2013-03-03 00:30:46 +0200

    sound: fixes

--- pdp/25/c/sound.cc
+++ pdp/25/c/sound.cc
@@ -23,10 +23,10 @@ int main() {
   s.insert(a, a + m);
   for (i = 0; i + m - 1 < n; ++i) {
     if (i) {
-      s.erase(a[i - 1]);
+      s.erase(s.lower_bound(a[i - 1]));
       s.insert(a[i + m - 1]);
     }
-    if (*s.end() - *s.begin() <= c) {
+    if ((*s.rbegin()) - (*s.begin()) <= c) {
       printf("%d\n", i + 1);
       ++j;
     }
