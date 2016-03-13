094035b5b67213a79af53bdf8dda65ecc021c231
Mario Saldinger <mariosaldinger@gmail.com>
2016-03-13 16:30:12 +0200

    Test: C++

---
+++ usaco/1.1/test.cc
@@ -0,0 +1,16 @@
+/*
+  ID: mariosa1
+  TASK: test
+  LANG: C++11
+*/
+#include <cstdio>
+
+int main() {
+  freopen("test.in", "r", stdin);
+  freopen("test.out", "w", stdout);
+
+  int a, b;
+  scanf("%d%d", &a, &b);
+  printf("%d\n", a + b);
+  return 0;
+}
