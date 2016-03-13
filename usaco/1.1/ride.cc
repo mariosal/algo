6f69254c141cd2a25c2335ea19c9d23110c81dc7
Mario Saldinger <mariosaldinger@gmail.com>
2016-03-13 16:50:17 +0200

    Ride: C++

--- usaco/1.1/ride.cc
+++ usaco/1.1/ride.cc
@@ -1,33 +1,29 @@
 /*
   ID: mariosa1
-  LANG: C
   TASK: ride
+  LANG: C++11
 */
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
 
-char hash( char *s ) {
-  char i, sum;
-  for ( i = 0, sum = 1; s[ i ]; i += 1 ) {
-    sum = ( sum * ( s[ i ] - 'A' + 1 ) ) % 47;
+static int Mod(char* str) {
+  int ret = 1;
+  for (size_t i = 0; str[i] != '\0'; ++i) {
+    char num = str[i] - 'A' + 1;
+    ret = (ret * (num % 47)) % 47;
   }
-  return sum;
+  return ret;
 }
 
 int main() {
-  char comet[ 7 ], group[ 7 ];
+  freopen("ride.in", "r", stdin);
+  freopen("ride.out", "w", stdout);
 
-  freopen( "ride.in", "r", stdin );
-  freopen( "ride.out", "w", stdout );
-
-  scanf( "%s\n%s", comet, group );
-
-  if ( hash( comet ) == hash( group ) ) {
-    puts( "GO" );
+  char comet[7], group[7];
+  scanf("%s%s", comet, group);
+  if (Mod(comet) == Mod(group)) {
+    printf("GO\n");
+  } else {
+    printf("STAY\n");
   }
-  else {
-    puts( "STAY" );
-  }
-
   return 0;
 }
