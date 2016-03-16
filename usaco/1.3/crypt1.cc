91fd0650f8ac363a65e12cb7a3ce6ca2851f84e6
Mario Saldinger <mariosaldinger@gmail.com>
2016-03-16 12:01:34 +0200

    Usaco: Crypt1: Rewrite

--- usaco/1.3/crypt1.cc
+++ usaco/1.3/crypt1.cc
@@ -1,49 +1,60 @@
 /*
-  PROG: crypt1
   ID: mariosa1
-  LANG: C++
+  TASK: crypt1
+  LANG: C++11
 */
 #include <cstdio>
-#include <cstdlib>
 #include <set>
-using namespace std;
-char check( set< char > S, int N ) {
-  char i, numbers[ 10 ];
-  sprintf( numbers, "%d", N );
-  for ( i = 0; numbers[ i ] != '\0'; ++i ) {
-    if ( S.find( numbers[ i ] - '0' ) == S.end() ) {
-      return 0;
+
+static int Length(int num) {
+  int ret = 0;
+  for (; num > 0; num /= 10) {
+    ++ret;
+  }
+  return ret;
+}
+
+static bool Valid(const std::set<int>& digits, int num) {
+  for (; num > 0; num /= 10) {
+    if (digits.find(num % 10) == digits.end()) {
+      return false;
     }
   }
-  return 1;
+  return true;
 }
+
 int main() {
-  int i, j, q, N;
-  set< char > S;
-  freopen( "crypt1.in", "r", stdin );
-  freopen( "crypt1.out", "w", stdout );
-  scanf( "%d", &N );
-  for ( i = 0; i < N; ++i ) {
-    scanf( "%d", &j );
-    S.insert( j );
+  freopen("crypt1.in", "r", stdin);
+  freopen("crypt1.out", "w", stdout);
+
+  int num_digits;
+  scanf("%d", &num_digits);
+
+  std::set<int> digits;
+  for (int i = 0; i < num_digits; ++i) {
+    int digit;
+    scanf("%d", &digit);
+    digits.insert(digit);
   }
-  q = 0;
-  for ( i = 100; i <= 999; ++i ) {
-    if ( check( S, i ) ) {
-      for ( j = 10; j <= 99; ++j ) {
-        if ( check( S, j ) &&
-           i * j / 10000 == 0 &&
-           i * ( ( int )j / 10 ) / 1000 == 0 &&
-           i * ( ( int )j % 10 ) / 1000 == 0 ) {
-          if ( check( S, i * ( ( int )j / 10 ) ) &&
-             check( S, i * ( ( int )j % 10 ) ) &&
-             check( S, i * j ) ) {
-            ++q;
+
+  int solutions = 0;
+  for (int i = 100; i <= 999; ++i) {
+    if (Valid(digits, i)) {
+      for (int j = 10; j <= 99; ++j) {
+        if (Valid(digits, j) &&
+            Length(i * j) == 4 &&
+            Length(i * (j / 10)) == 3 &&
+            Length(i * (j % 10)) == 3) {
+          if (Valid(digits, i * j) &&
+              Valid(digits, i * (j / 10)) &&
+              Valid(digits, i * (j % 10))) {
+            ++solutions;
           }
         }
       }
     }
   }
-  printf( "%d\n", q );
+
+  printf("%d\n", solutions);
   return 0;
 }
