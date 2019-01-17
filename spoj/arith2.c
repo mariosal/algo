34ceb5230814637e9714b1e923826660aa611b05
Mario Saldinger <mariosal@skroutz.gr>
2019-01-17 12:16:14 +0200

    Spoj: Correct seg-fault in ARITH2

--- spoj/arith2.c
+++ spoj/arith2.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 int main() {
-  char operator;
+  char operator[2];
   long long n, a, res;
 
   scanf( "%lld", &n );
   while ( n-- ) {
-    operator = 0;
+    operator[0] = '\0';
     scanf( "%lld", &res );
     while ( 1 ) {
-      scanf( "%s", &operator );
-      if ( operator == '=' ) {
+      scanf( "%s", operator );
+      if ( operator[0] == '=' ) {
         break;
       }
 
       scanf( "%lld", &a );
-      switch ( operator ) {
+      switch ( operator[0] ) {
         case '+':
           res += a;
           break;
